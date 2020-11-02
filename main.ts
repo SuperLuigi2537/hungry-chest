332e889603438f76f874f704ceaa15acf5d6eff3
SuperLuigi2537 <70593455+SuperLuigi2537@users.noreply.github.com>
2020-11-02 18:12:20 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+namespace SpriteKind {
+    export const pizza = SpriteKind.create()
+}
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.stopAnimation(animation.AnimationTypes.All, character)
     character.setImage(img`
@@ -57,10 +60,10 @@ controller.A.onEvent(ControllerButtonEvent.Released, function () {
         b b b b b b b d d b b b b b b b 
         c b b b b b b c c b b b b b b c 
         c c c c c c b c c b c c c c c c 
-        b e e e e e c b b c e e e e e b 
-        b e e e e e e e e e e e e e e b 
-        b c e e e e e e e e e e e e c b 
-        b b b b b b b b b b b b b b b b 
+        b e 1 e e 2 c b b c 1 e 1 e e b 
+        b e 1 e e 2 2 e e e e e 1 e e b 
+        b c e e e 2 2 e e e e e e e c b 
+        b b b b b 2 b b b b b b b b b b 
         . b b . . . . . . . . . . b b . 
         `,img`
         . b b b b b b b b b b b b b b . 
@@ -104,10 +107,21 @@ controller.A.onEvent(ControllerButtonEvent.Released, function () {
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
     statusbar.value += 5
+    music.magicWand.play()
     pause(100)
+    burgerlife += 1
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.pizza, function (sprite, otherSprite) {
+    statusbar.value += 10
+    music.magicWand.play()
+    pause(100)
+    pizzalife += 1
+    info.changeScoreBy(1)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     statusbar.value += -5
+    music.pewPew.play()
     pause(100)
 })
 let statusbar: StatusBarSprite = null
@@ -131,8 +145,44 @@ let burger = sprites.create(img`
     . e e b b 4 4 4 4 4 4 4 4 e e . 
     . . . c c c c c e e e e e . . . 
     `, SpriteKind.Food)
+let pizzafood = sprites.create(img`
+    .............beebbbb............
+    ............eebbbb4bb...........
+    ............eb344bb4bb..........
+    ............e44334bb4bb.........
+    ............eb433344b4be........
+    ............4eb43344444be.......
+    ...........bd4eb43333344bb......
+    ..........b455d4443333444bb.....
+    ..........4d5555d444333444bb....
+    .........4555555dd4b4443444be...
+    ........bd5555d555d4bb444444ee..
+    ........b55ddd665555bb4b44444ee.
+    .......bd5555677655554ebb44444eb
+    .......43222558855555d4eeb44b4ee
+    ......b422332ddd555222d4eebbb4be
+    ......be22232ed55522332db4ebbbbe
+    .....bde22222e555e22232edd4bbbbe
+    .....b52e222e3555e22222eddd4ebee
+    ....bd552eee355552e222e355544eee
+    ....665dd5555555552eee355dd4deee
+    ...6776555555555555555551554d4ee
+    ...4885222555dddd6655551544d4eee
+    ..b45522332555dd677611d444ddeee.
+    ..4d5222232e55555881d44ddd4eee..
+    .bdd5e22222e555115114d54d4ee....
+    .b55d2e222e351144d1d55eeee......
+    bd5ddd2eee3d444555dd4e..........
+    b555115dddd55d544eede...........
+    4511d444d5544ee...4de...........
+    41d4555d4ee........44...........
+    41554eede.......................
+    44ee...4e.......................
+    `, SpriteKind.pizza)
+let burgerlife = 0
+let pizzalife = 0
 scene.setBackgroundColor(12)
-tiles.setTilemap(tiles.createTilemap(hex`1000100005050505050505050505050505050505050505050505050505050505050505050502020202020202020202020202020503060606060606060606060606060604030606060606060606060606060606040306060606060606060606060606060403060606060606060606060606060604030606060606060606060606060606040306060606060606060606060606060403060606060606060606060606060604010101010101010101010101010101010509050505050505050505050505050505050505050505000505050505050505050505050505050505050505050705050505080505050505050505050505050505050505050505050505050505050505`, img`
+tiles.setTilemap(tiles.createTilemap(hex`10001000030303030303030303030303030303030303030303030303030303030303030303020202020202020202020202020203060a0a0a0a0a0a0a0a0a0a0a0a0a0a08060a0a0a0a0a0a0a0a0a0a0a0a0a0a08060a0a0a0a0a0a0a0a0a0a0a0a0a0a08060a0a0a0a0a0a0a0a0a0a0a0a0a0a08060a0a0a0a0a0a0a0a0a0a0a0a0a0a08060a0a0a0a0a0a0a0a0a0a0a0a0a0a0805090909090909090909090909090907010101010101010101010101010101010304030303030303030303030303030303030303030303000303030303030303030303030303030303030303030403030303040303030303030303030303030303030303030303030303030303030303`, img`
     2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
     2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
     2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
@@ -149,7 +199,7 @@ tiles.setTilemap(tiles.createTilemap(hex`100010000505050505050505050505050505050
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
-    `, [myTiles.transparency16,sprites.dungeon.darkGroundNorth,sprites.dungeon.darkGroundSouth,sprites.dungeon.darkGroundEast,sprites.dungeon.darkGroundWest,sprites.dungeon.darkGroundCenter,sprites.castle.tilePath5,sprites.dungeon.collectibleBlueCrystal,sprites.dungeon.collectibleInsignia,sprites.dungeon.collectibleRedCrystal], TileScale.Sixteen))
+    `, [myTiles.transparency16,sprites.dungeon.darkGroundNorth,sprites.dungeon.darkGroundSouth,sprites.dungeon.darkGroundCenter,sprites.dungeon.collectibleInsignia,sprites.dungeon.purpleOuterSouthEast,sprites.dungeon.purpleOuterWest1,sprites.dungeon.purpleOuterSouthWest,sprites.dungeon.purpleOuterEast1,sprites.dungeon.purpleOuterSouth1,sprites.dungeon.floorLight2], TileScale.Sixteen))
 character = sprites.create(img`
     . b b b b b b b b b b b b b b . 
     b e 4 4 4 4 4 4 4 4 4 4 4 4 4 b 
@@ -197,6 +247,7 @@ let googlie = sprites.create(img`
 controller.moveSprite(character)
 scene.cameraFollowSprite(character)
 tiles.placeOnRandomTile(burger, sprites.dungeon.collectibleInsignia)
+tiles.placeOnRandomTile(pizzafood, sprites.dungeon.collectibleInsignia)
 tiles.placeOnRandomTile(googlie, myTiles.transparency16)
 animation.runImageAnimation(
 character,
@@ -275,3 +326,11 @@ true
 tiles.placeOnTile(character, tiles.getTileLocation(4, 10))
 statusbar = statusbars.create(20, 2, StatusBarKind.Health)
 statusbar.attachToSprite(character)
+game.onUpdate(function () {
+    if (burgerlife == 20) {
+        burger.destroy()
+    }
+    if (pizzalife == 100) {
+        pizzafood.destroy()
+    }
+})
